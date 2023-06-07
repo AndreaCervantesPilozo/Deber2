9e704bbfa0144ed93358a3338a8472613f452748
AndreaCervantesPilozo <133895031+AndreaCervantesPilozo@users.noreply.github.com>
2023-06-07 01:14:20 -0500

    Update admin.py

--- sap/personas/admin.py
+++ sap/personas/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+<! DOCTYPE html>
+<.html >
+<cabeza>
+    <meta charset="UTF-8">
+    <title>Sistemas de Administraciopn de Personas</title>
+</cabeza>
+<cuerpo>
+<h1>Sistemas de Administración de Personas</h1>
+<p>Cantidad de personas: {{cantidad}}</p>
+
+{% para persona en personas %}
+    <p>{{persona.nombre}}   {{persona.apellido}}    {{persona.email}}</p>
+
+{% endfor %}
+
+<Br>
+<Br>
+<Br>
+<Br>
+
+{% para nombre en nombres_personas %}
+    <p>{{nombre}}</p>
+
+{% endfor %}
+</cuerpo>
+</.html>
