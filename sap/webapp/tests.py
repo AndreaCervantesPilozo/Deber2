121e667b18ff71d7ab511b56da68ce2a5189b5f2
AndreaCervantesPilozo <133895031+AndreaCervantesPilozo@users.noreply.github.com>
2023-06-07 01:09:02 -0500

    Update tests.py

--- sap/webapp/tests.py
+++ sap/webapp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+class Migration(migrations. Migración):
+
+    inicial = Verdadero
+
+    Dependencias = [
+    ]
+
+    Operaciones = [
+        migraciones. CreateModel(
+            name='Persona',
+            Campos=[
+                ('id', modelos. BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('nombre', modelos. CharField(max_length=50)),
+                ('apellido', modelos. CharField(max_length=50)),
+                ('email', modelos. CharField(max_length=50)),
+                ('activo', modelos. BooleanField(default=True)),
+            ],
+        ),
+    ]
